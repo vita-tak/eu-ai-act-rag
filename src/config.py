042bcd262b3175a0e632b998e02e218e22d0809f
vita-tak <isacbroman@gmail.com>
2026-09-01 01:38:09 +0200

    add workspace ID to config

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+ANTHROPIC_WORKSPACE_ID = os.getenv("ANTHROPIC_WORKSPACE_ID")
 
 # Paths
 DATA_PATH = "data/eu_ai_act.txt"
